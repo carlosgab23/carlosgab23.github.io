25134bf175f6f32eacf873112655f7e48c3770ac
carlosgab23 <153801451+carlosgab23@users.noreply.github.com>
2023-12-18 15:15:02 -0600

    Update conexion.php
    Initial commit

--- conexion.php
+++ conexion.php
@@ -1,19 +1,23 @@
 <?php
-    $mysqlConect=new mysql('localhost','root','','pagina');
-    if (!$mysqlConect) {
-        echo "Error al conectar la base de datos";
-    }
-    $Nombre=$_POST['Nombre'];
-    $ApePat=$_POST['ApeP'];
-    $Numero=$_POST['Numero'];
-    $Producto=$_POST['producto'];
-    $Codigo=$_POST['codigo'];
-    $Talla=$_POST['talla'];
 
-    $insertar = "INSERT INTO datos(Nombre,ApePat,Numero,Producto,Codigo,Talla) VALUES ('$Nombre','$ApePat','$Numero','$Producto','$Codigo','$Talla')";
-    if (mysql_query($mysqliConect,$insertar)){
-        echo "";
-    }else{
-        echo "Error al guardar datos";
-    }
-?>
+$link = mysql_connect("localhost","root","") or die ("<h2>No se encuentra el servidor</h2>");;
+$db = mysql_select_db ("pagina",$link) or die ("<h2>Error de conexion</h2>");
+
+$Nombre=$_POST['Nombre'];
+$ApePat=$_POST['ApeP'];
+$Numero=$_POST['Numero'];
+$Producto=$_POST['producto'];
+$Codigo=$_POST['codigo'];
+$Talla=$_POST['talla'];
+
+$req = (strlen($Nombre)*strlen($ApePat)*strlen($Numero)*strlen($Producto)*strlen($Codigo)*strlen($Talla)) or die("No se han llenado los campos <br><br><a href='pedido.html'>")
+
+mysql_query("INSERT INTO datos VALUES ('','$Nombre','$ApePat','$Numero','$Producto','$Codigo','$Talla')",$link) or die ("<h2>Error de envio</h2>");
+
+echo '
+    <h2>Registro Completo</h2>
+    <h5>Gracias por su pedido, nos pondremos en contacto con usted</h5>
+    ';
+
+    ?>
+
