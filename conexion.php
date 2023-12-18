6ba004c694d11b94c241b7cf104507a95324bf0c
carlosgab23 <153801451+carlosgab23@users.noreply.github.com>
2023-12-18 15:58:53 -0600

    Update conexion.php
    Initial commit

--- conexion.php
+++ conexion.php
@@ -1,23 +1,25 @@
 <?php
+	//conexion con la base de datos y el servidor
+	$link = mysql_connect("localhost","root","") or die("<h2>No se encuentra el servidor</h2>");
+	$db = mysql_select_db("pagina",$link) or die("<h2>Error de Conexion</h2>");
 
-$link = mysql_connect("localhost","root","") or die ("<h2>No se encuentra el servidor</h2>");;
-$db = mysql_select_db ("pagina",$link) or die ("<h2>Error de conexion</h2>");
+	//obtenemos los valores del formulario
+	$Nombre=$_POST['Nombre'];
+	$ApePat=$_POST['ApeP'];
+	$Numero=$_POST['Numero'];
+	$Producto=$_POST['producto'];
+	$Codigo=$_POST['codigo'];
+	$Talla=$_POST['talla'];
 
-$Nombre=$_POST['Nombre'];
-$ApePat=$_POST['ApeP'];
-$Numero=$_POST['Numero'];
-$Producto=$_POST['producto'];
-$Codigo=$_POST['codigo'];
-$Talla=$_POST['talla'];
+	//Obtiene la longitus de un string
+	$req = (strlen($Nombre)*strlen($ApePat)*strlen($Numero)*strlen($Producto)*strlen($Codigo)*strlen($Talla)) or die("No se han llenado todos los campos");
 
-$req = (strlen($Nombre)*strlen($ApePat)*strlen($Numero)*strlen($Producto)*strlen($Codigo)*strlen($Talla)) or die("No se han llenado los campos <br><br><a href='pedido.html'>")
-
-mysql_query("INSERT INTO datos VALUES ('','$Nombre','$ApePat','$Numero','$Producto','$Codigo','$Talla')",$link) or die ("<h2>Error de envio</h2>");
-
-echo '
-    <h2>Registro Completo</h2>
-    <h5>Gracias por su pedido, nos pondremos en contacto con usted</h5>
-    ';
-
-    ?>
-
+	//ingresamos la informacion a la base de datos
+	mysql_query("INSERT INTO datos VALUES('','$Nombre','$ApePat','$Numero','$Producto','$Codigo','$Talla')",$link) or die("<h2>Error Guardando los datos</h2>");
+	echo'
+		<script>
+			alert("Registro Exitoso");
+			location.href="pedido.html";
+		</script>
+	'
+?>
