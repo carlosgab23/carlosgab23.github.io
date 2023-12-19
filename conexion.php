f234627ea50b14447960e32519e077ef8d997a6a
carlosgab23 <153801451+carlosgab23@users.noreply.github.com>
2023-12-19 01:36:24 -0600

    Update conexion.php
    Initial commit

--- conexion.php
+++ conexion.php
@@ -1,25 +1,34 @@
 <?php
 	//conexion con la base de datos y el servidor
 	$link = mysql_connect("localhost","root","") or die("<h2>No se encuentra el servidor</h2>");
-	$db = mysql_select_db("pagina",$link) or die("<h2>Error de Conexion</h2>");
+	$db = mysql_select_db("formulario",$link) or die("<h2>Error de Conexion</h2>");
 
 	//obtenemos los valores del formulario
-	$Nombre=$_POST['Nombre'];
-	$ApePat=$_POST['ApeP'];
-	$Numero=$_POST['Numero'];
-	$Producto=$_POST['producto'];
-	$Codigo=$_POST['codigo'];
-	$Talla=$_POST['talla'];
+	$nombres = $_POST['nombreuser'];
+	$apellidos = $_POST['apellidosuser'];
+	$email = $_POST['emailuser'];
+	$pass = $_POST['pass'];
+	$rpass = $_POST['rpass'];
 
 	//Obtiene la longitus de un string
-	$req = (strlen($Nombre)*strlen($ApePat)*strlen($Numero)*strlen($Producto)*strlen($Codigo)*strlen($Talla)) or die("No se han llenado todos los campos");
+	$req = (strlen($nombres)*strlen($apellidos)*strlen($email)*strlen($pass)*strlen($rpass)) or die("No se han llenado todos los campos");
+
+	//se confirma la contraseña
+	if ($pass != $rpass) {
+		die('Las contraseñas no coinciden, Verifique <br /> <a href="index.html">Volver</a>');
+	}
+
+	//se encripta la contraseña
+	$contraseñaUser = md5($pass);
 
 	//ingresamos la informacion a la base de datos
-	mysql_query("INSERT INTO datos VALUES('','$Nombre','$ApePat','$Numero','$Producto','$Codigo','$Talla')",$link) or die("<h2>Error Guardando los datos</h2>");
+	mysql_query("INSERT INTO datos VALUES('','$nombres','$apellidos','$email','$contraseñaUser')",$link) or die("<h2>Error Guardando los datos</h2>");
 	echo'
 		<script>
 			alert("Registro Exitoso");
-			location.href="pedido.html";
+			location.href="index.html";
 		</script>
 	'
+
+
 ?>
